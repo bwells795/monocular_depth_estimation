c17c01951fe8baeba468d400828e2aa5ed4bc3ec
Michael <michael.j.baker98@gmail.com>
2025-11-06 17:24:48 -0500

    committing changes before trying to merge

---
+++ losses/LMR.py
@@ -0,0 +1,55 @@
+"""
+This file contains a library of loss methods which can be used to optimize the learned mask regularizer (LMR)
+
+"""
+
+import torch.nn as nn
+import torch
+
+
+class LMRLoss(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        # There are a couple different loss types we may want to do here
+
+    def forward(
+        self,
+        net_mask: torch.Tensor,
+        depth_hat: torch.Tensor,
+        depth: torch.Tensor,
+        k: int,
+    ):
+        """
+        Implement this from the paper
+        """
+        return self.info_gain_loss(net_mask, depth_hat, depth, k)
+
+    def info_gain_loss(
+        self,
+        net_mask: torch.Tensor,
+        depth_hat: torch.Tensor,
+        depth: torch.Tensor,
+        k: int,
+    ):
+        """Original idea usees information gain like that is used to build decision trees"""
+
+        # calculate d_hat - d
+        diff = torch.abs(depth_hat - depth)
+
+        # predict liklihood of next iteration via gaussian activation function
+        gauss_pred = self.gaussian_activation(diff)
+
+        # find top-k pixels
+        assert k > 0
+        _, top_k_inds = torch.topk(gauss_pred, k)
+
+        net_mask = net_mask.to(torch.bool)
+        pred_mask = top_k_inds.to(torch.bool)
+
+        iou = torch.sum(net_mask * pred_mask) / torch.sum(net_mask + pred_mask)
+        return torch.log(1 / iou)
+
+    def gaussian_activation(self, x):
+        """Calcualte gaussian activation function e^(-x^2)"""
+        return torch.exp(-1 * x ^ 2)
